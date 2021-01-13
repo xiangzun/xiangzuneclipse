604d050875665c74f6b632bb434e46c95af4169a
xz <xz@.com>
2021-01-13 18:41:41 +0800

    oooo

--- src/main/java/com/insigma/xiangzun/com/App.java
+++ src/main/java/com/insigma/xiangzun/com/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!hkhhhhh" );
 
         System.out.println( "Hello World!llllll" );
 
